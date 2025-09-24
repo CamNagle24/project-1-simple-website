c5662da810735d6d094943626a878ef366f162f1
Cameron Nagle <camnag01@gmail.com>
2025-09-24 16:19:45 -0500

    new

--- javascript.js
+++ javascript.js
@@ -17,6 +17,28 @@ while(reagan<10){
     reagan++
 }
 
+console.log('Here\'s a multiplication table method rendered in the console with a for loop:\n')
+console.log('This demos variables, concatenation, for loops, console.logging and methods, along with method parameters')
+let timesTable = (to) => {
+    console.log('Here\'s a times table to ' + to + '\n')
+    if (Number.isInteger(to)) {
+        //rows iterator
+        for (let i = 1; i < to; i++) {
+            let row = ""
+            //columns iterator
+            for (let j = 1; j < to; j++) {
+                row += `${i * j}\t`
+            }
+            console.log(row)
+        }
+    } else {
+        console.log(`Sorry, ${to} isn't a number!` )
+    }
+}
+
+timesTable(4)
+
+
 console.log('Here\'s a for loop showing 1...10:\n')
 let nums=[...Array(10).keys()]
 nums.forEach(n=>{
